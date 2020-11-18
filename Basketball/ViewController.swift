a93a3e09127b9cb35e07acb11233aafc1ed084d3
bence-t0th <bence.toth.edu@icloud.com>
2020-11-18 21:02:21 +0100

    Add hoop and ball

---
+++ Basketball/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  Basketball
+//
+//  Created by Bence Tóth on 2020. 11. 18..
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        addBackboard()
+        
+        registerGestureRecognizer()
+    }
+    
+    func addBackboard() {
+        guard let backboardScene = SCNScene(named: "art.scnassets/hoop.scn") else {
+            return
+        }
+        
+        guard let backboardNode = backboardScene.rootNode.childNode(withName: "backboard", recursively: false) else {
+            return
+        }
+        
+        backboardNode.position = SCNVector3(x: 0, y: 0.5, z: -3)
+        
+        sceneView.scene.rootNode.addChildNode(backboardNode)
+    }
+    
+    func registerGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(gestureRecognizer: UIGestureRecognizer) {
+        guard let sceneView = gestureRecognizer.view as? ARSCNView else {
+            return
+        }
+        
+        guard let centerPoint = sceneView.pointOfView else {
+            return
+        }
+        
+        let cameraTransform = centerPoint.transform
+        let cameraLocation = SCNVector3(x: cameraTransform.m41, y: cameraTransform.m42, z: cameraTransform.m43)
+        let cameraOrientation = SCNVector3(-cameraTransform.m31, -cameraTransform.m32, -cameraTransform.m33)
+        
+        let cameraPosition = SCNVector3Make(cameraLocation.x + cameraOrientation.x, cameraLocation.y + cameraOrientation.y, cameraLocation.z + cameraOrientation.z)
+        
+        let ball = SCNSphere(radius: 0.15)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "basketballSkin.png")
+        ball.materials = [material]
+        
+        let ballNode = SCNNode(geometry: ball)
+        ballNode.position = cameraPosition
+        
+        sceneView.scene.rootNode.addChildNode(ballNode)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
